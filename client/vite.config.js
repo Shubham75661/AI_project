44c895234e1509c512a24b13961a97947f4f6159
shubham <shubham75661@gmail.com>
2023-04-10 23:26:00 +0530

    #END for today

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost'
+    }
+  },
   plugins: [react()],
 })
